63f027944132e5a1f0878b9d16da33fe7a760c88
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-06-03 16:55:55 +0300

    fixed extra space

--- src/main/java/steammachinist/proxy/ProxyUsageExample.java
+++ src/main/java/steammachinist/proxy/ProxyUsageExample.java
@@ -5,7 +5,7 @@ public class ProxyUsageExample {
     public static void run() {
         System.out.println("\n\nProxy usage example:");
         DatabaseConnectionImpl database = new DatabaseConnectionImpl("prod-db");
-        DatabaseConnectionProxy databaseProxy = new DatabaseConnectionProxy(database, " localhost:5050/");
+        DatabaseConnectionProxy databaseProxy = new DatabaseConnectionProxy(database, "localhost:5050/");
         databaseProxy.connect();
     }
 }
